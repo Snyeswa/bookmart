1e533a698955dda6c9f24ed57548d54867700b5f
Sylvester Nyeswa <sylvesternyeswa.student@careerdevs.com>
2019-09-08 17:16:20 -0400

    DOM Traversaling-1

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
-const banner = document.querySelector('#page-banner');
+const bookList = document.querySelector('#book-list');
 
-console.log('#page-banner node type is:', banner.nodeType);
-console.log('#page-banner node name is:', banner.nodeName);
-console.log('#page-banner has child nodes:', banner.hasChildNodes());
+console.log('the parent node is:', bookList.parentNode);
+console.log('the parent element is:', bookList.parentElement.parentElement);
 
-const cloneBanner = banner.cloneNode(true);
-console.log(clonedBanner);
+console.log(bookList.children);
+console.log('all node children:');
+Array.from(bookList.childNodes).forEach(function(node){
+  console.log(node);
+});
 
+console.log('all element children:');
+Array.from(bookList.children).forEach(function(node){
+  console.log(node);
+});
+
+const titles = bookList.querySelectorAll('.name');
+
+console.log('Book titles:');
+Array.from(titles).forEach(function(title){
+  console.log(title.textContent);
+});
