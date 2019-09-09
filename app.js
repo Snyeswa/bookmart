456f6b553c80b11350941554a7b43b87860d3f3c
Sylvester Nyeswa <sylvesternyeswa.student@careerdevs.com>
2019-09-08 22:27:15 -0400

    Add book

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const list = document.querySelector('#book-list ul');
 
-// delete books
+//  delete books
 list.addEventListener('click', function(e){
 	if(e.target.className == 'delete'){
 		const li = e.target.parentElement;
-		list.removeChild(li);
+		list.removeChil(li);
 	}
 });
+
+// add book-list
+
+const addForm = document.forms('add-book');
+addForm.addEventListener('submit', function(e){
+	e.parentDefault();
+	const value = addForm.querySelector('input[type="text"]').value;
+	console.log(value);
+})
