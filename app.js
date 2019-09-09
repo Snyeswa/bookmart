c1daf61ea1f0bc1dd78b6d4b6b18900dda0d91cb
Sylvester Nyeswa <sylvesternyeswa.student@careerdevs.com>
2019-09-08 22:17:01 -0400

    Use Dom to delete an event for the app

--- app.js
+++ app.js
@@ -1,22 +1,9 @@
-var btns = document.querySelectorAll('#book-list.delete');
+const list = document.querySelector('#book-list ul');
 
-Array.from(btns).forEach(function(btn){
-	btn.addEventListener('click', function(e){
+
+list.addEventListener('click', function(e){
+	if(e.target.className == 'delete'){
 		const li = e.target.parentElement;
-
-		li.parentNode.removeChild(li);
-    
-    });
+		list.removeChild(li);
+	}
 });
-
-const link = document.querySelectorAll('#page-banner');
-link.addEventListener('click', function(e){
-	e.preventDefault();
-	console.log('navigation to', e.target.textContent, 'was prevented');
-})
-// prevent default behaviour
-const link = document.querySelector('#page-banner a');
-link.addEventListener('click', function(e){
-    e.preventDefault();
-    console.log('Navigation to', e.target.textContent, 'was prevented');
-})
